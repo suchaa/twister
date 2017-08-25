4291523960c42b1828b70b3358bc10713b74e628
testgitconfig <testgitconfig>
2017-08-25 11:16:06 +0700

    prevent access without signin

--- modules/tweets/client/config/tweets.client.routes.js
+++ modules/tweets/client/config/tweets.client.routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('tweets').config(['$stateProvider',
+angular.module('tweets')
+.config(['$stateProvider',/* ทำงานตอนเริ่มต้น */
   function ($stateProvider) {
 
     $stateProvider
@@ -13,4 +14,22 @@ angular.module('tweets').config(['$stateProvider',
         templateUrl: 'modules/tweets/views/feed/feed.client.view.html'
       });
   }
+])
+
+/* ตอนที่เริ่มทำงาน */
+.run([
+  '$rootScope',
+  '$state',
+  'Authentication',
+  function($rootScope, $state, Authentication){/* $on ใช้ detect เวลามี event เกิดขึ้น */
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromParams){
+      if( ((toState.name !== 'signup') && (toState.name !== 'signin')) 
+      && !Authentication.user){
+        event.preventDefault(); /*  เผื่อมีการโหลดหน้าไว้แล้ว ตัวนี้จะบอกว่าไม่ต้องเดี๋ยวฉันจัดการเอง จัดการไปที่ signup */
+        $state.go('signup');
+      }
+    });
+  }
 ]);
+
+
--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -13,6 +13,15 @@ angular.module('users').config(['$stateProvider',
       url: '/signin',
       templateUrl: 'modules/users/views/authentication/signin.client.view.html'
     })
+    
+    .state('authentication.signup', {
+      url: '/signup',
+      templateUrl: 'modules/users/views/authentication/signup.client.view.html'
+    })
+    .state('authentication.signin', {
+      url: '/signin?err',
+      templateUrl: 'modules/users/views/authentication/signin.client.view.html'
+    })
 
     
      /*  .state('settings', {
